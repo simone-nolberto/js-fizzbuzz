e6e8091ce8c77dedc17db712e1da517a56badd6d
simone-nolberto <simone.nolberto@outlook.com>
2024-02-21 16:32:48 +0100

    update
    main structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,22 @@
-console.log("it works");
+const listElement = document.querySelector(".list");
+const fizz = "Fizz";
+const buzz = "Buzz";
+
+for (let x = 1; x <= 100; x++) {
+
+    if (x % 3 === 0) {
+        console.log(fizz)
+        listElement.innerHTML += `<li square f-square> ${fizz} </li>`;;
+    } else if (x % 5 === 0) {
+        console.log(buzz);
+        listElement.innerHTML += `<li square b-square> ${buzz} </li>`;
+    } else if (x % 3 === 0 && x % 5 === 0) {
+        console.log(fizz + buzz);
+        listElement.innerHTML += `<li square fb-square> ${fizz + buzz} </li>`;
+    } else {
+        console.log(x);
+        listElement.innerHTML += `<li square> ${x} </li>`;
+    }
+
+}
 
