6c067fe00c118bb20de62334775c1fbb57f9852a
simone-nolberto <simone.nolberto@outlook.com>
2024-02-21 17:52:23 +0100

    update
    main structure working

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,18 +4,18 @@ const buzz = "Buzz";
 
 for (let x = 1; x <= 100; x++) {
 
-    if (x % 3 === 0) {
-        console.log(fizz)
-        listElement.innerHTML += `<li square f-square> ${fizz} </li>`;;
+    if (x % 3 === 0 && x % 5 === 0) {
+        console.log(fizz + buzz)
+        listElement.innerHTML += `<li class="square fb"> ${fizz + buzz} </li>`;
+    } else if (x % 3 === 0) {
+        console.log(fizz);
+        listElement.innerHTML += `<li class="square f"> ${fizz} </li>`;
     } else if (x % 5 === 0) {
         console.log(buzz);
-        listElement.innerHTML += `<li square b-square> ${buzz} </li>`;
-    } else if (x % 3 === 0 && x % 5 === 0) {
-        console.log(fizz + buzz);
-        listElement.innerHTML += `<li square fb-square> ${fizz + buzz} </li>`;
+        listElement.innerHTML += `<li class="square b"> ${buzz} </li>`;
     } else {
         console.log(x);
-        listElement.innerHTML += `<li square> ${x} </li>`;
+        listElement.innerHTML += `<li class="square"> ${x} </li>`;
     }
 
 }
